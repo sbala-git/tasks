070cf6a2d146e89de75b40b102e222612057d653
Bala <sbala@udel.edu>
2024-09-24 14:13:35 -0400

    Bala Saravanan

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return ((temperature - 32) * 5) / 9;
 }
 
 /**
@@ -12,7 +12,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    return (
+        (first > 0 ? first : 0) +
+        (second > 0 ? second : 0) +
+        (third > 0 ? third : 0)
+    );
 }
 
 /**
@@ -20,7 +24,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,14 +32,19 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
  * Consumes a word (a string) and returns either `true`, `false`, or `null`. If the string
  * is "yes" (upper or lower case), then return `true`. If the string is "no" (again, either
- * upper or lower case), then return `false`. Otherwise, return `null`.
+ * upper or lowergit case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    const lowercase = word.toLowerCase();
+    return (
+        lowercase === "yes" ? true
+        : lowercase === "no" ? false
+        : null
+    );
 }
